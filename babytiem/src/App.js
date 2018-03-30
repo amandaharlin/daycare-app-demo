c59f9aafa14f81c15f184df865c496c62de144d3
Amanda Harlin <amanda.n.harlin@gmail.com>
2018-03-29 22:48:42 -0500

    breaking apart code into components

--- babytiem/src/App.js
+++ babytiem/src/App.js
@@ -17,96 +17,80 @@ import { mockPotties } from './mockData/mockPotties';
 import { pottyOptions } from './pottyOptions';
 
 class App extends Component {
+  renderHeader = () => {
+    return (
+      <Header as="h2" icon textAlign="center">
+        <Icon name="theme" circular basic color="teal" />
+        <Header.Content>Potty Time</Header.Content>
+      </Header>
+    );
+  };
+
+  renderControlPanel = () => {
+    return (
+      <Form>
+        <Dropdown placeholder="Potty Time" selection options={pottyOptions} />
+
+        <Button type="submit">+</Button>
+      </Form>
+    );
+  };
+
+  renderPottyList = () => {
+    return (
+      <List relaxed size="big">
+        <Divider horizontal>Today</Divider>
+        <List.Item>
+          <List.Icon color="teal" circular inverted name="theme" size="big" />
+          <List.Content verticalAlign="middle">
+            <List.Header>Wet Diaper</List.Header>
+            <List.Description>11:36am</List.Description>
+            <List.Description>Pull Ups leaked</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Icon color="teal" circular inverted name="theme" size="big" />
+          <List.Content verticalAlign="middle">
+            <List.Header>BM Diaper</List.Header>
+            <List.Description>2:36pm</List.Description>
+          </List.Content>
+        </List.Item>
+        <Divider horizontal>March 23</Divider>
+        <List.Item>
+          <List.Icon color="teal" circular inverted name="theme" size="big" />
+          <List.Content verticalAlign="middle">
+            <List.Header>Wet Diaper</List.Header>
+            <List.Description>4:40pm</List.Description>
+          </List.Content>
+        </List.Item>
+        <List.Item>
+          <List.Icon color="teal" circular inverted name="theme" size="big" />
+          <List.Content verticalAlign="middle">
+            <List.Header>Potty - Tried</List.Header>
+            <List.Description>4:44pm</List.Description>
+          </List.Content>
+        </List.Item>
+        <Divider horizontal>March 22</Divider>
+        <List.Item>
+          <List.Icon color="teal" circular inverted name="theme" size="big" />
+          <List.Content verticalAlign="middle">
+            <List.Header>Dry Diaper</List.Header>
+            <List.Description>1:40pm</List.Description>
+          </List.Content>
+        </List.Item>
+      </List>
+    );
+  };
+
   render() {
     return (
       <div className="App">
         <Container>
-          <Header as="h2" icon textAlign="center">
-            <Icon name="theme" circular basic color="teal" />
-            <Header.Content>Potty Time</Header.Content>
-          </Header>
+          {this.renderHeader()}
           <Divider hidden />
-          <Form>
-            <Dropdown
-              placeholder="Potty Time"
-              selection
-              options={pottyOptions}
-            />
-
-            <Button type="submit">+</Button>
-          </Form>
+          {this.renderControlPanel()}
           <Divider hidden />
-          <Divider horizontal>Today</Divider>
-          <List relaxed size="big">
-            <List.Item>
-              <List.Icon
-                color="teal"
-                circular
-                inverted
-                name="theme"
-                size="big"
-              />
-              <List.Content verticalAlign="middle">
-                <List.Header>Wet Diaper</List.Header>
-                <List.Description>11:36am</List.Description>
-                <List.Description>Pull Ups leaked</List.Description>
-              </List.Content>
-            </List.Item>
-            <List.Item>
-              <List.Icon
-                color="teal"
-                circular
-                inverted
-                name="theme"
-                size="big"
-              />
-              <List.Content verticalAlign="middle">
-                <List.Header>BM Diaper</List.Header>
-                <List.Description>2:36pm</List.Description>
-              </List.Content>
-            </List.Item>
-            <Divider horizontal>March 23</Divider>
-            <List.Item>
-              <List.Icon
-                color="teal"
-                circular
-                inverted
-                name="theme"
-                size="big"
-              />
-              <List.Content verticalAlign="middle">
-                <List.Header>Wet Diaper</List.Header>
-                <List.Description>4:40pm</List.Description>
-              </List.Content>
-            </List.Item>
-            <List.Item>
-              <List.Icon
-                color="teal"
-                circular
-                inverted
-                name="theme"
-                size="big"
-              />
-              <List.Content verticalAlign="middle">
-                <List.Header>Potty - Tried</List.Header>
-                <List.Description>4:44pm</List.Description>
-              </List.Content>
-            </List.Item>
-            <Divider horizontal>March 22</Divider>
-            <List.Item>
-              <List.Icon
-                color="teal"
-                circular
-                inverted
-                name="theme"
-                size="big"
-              />
-              <List.Content verticalAlign="middle">
-                <List.Header>Dry Diaper</List.Header>
-                <List.Description>1:40pm</List.Description>
-              </List.Content>
-            </List.Item>
-          </List>
+          {this.renderPottyList()}
         </Container>
       </div>
     );
--- babytiem/src/mockData/mockPotties.js
+++ babytiem/src/mockData/mockPotties.js
@@ -3,32 +3,84 @@ const mockPotties = [
     id: 1,
     activity: 'potty',
     icon: 'theme',
-    date: '03-26-2018',
-    time: '12:01pm',
+    date: new Date(),
     type: 'wet diaper'
   },
   {
     id: 2,
     activity: 'potty',
     icon: 'fire',
-    date: '03-26-2018',
-    time: '2:00pm',
+    date: new Date(),
     type: 'BM diaper'
   },
   {
     id: 3,
     activity: 'potty',
     icon: 'theme',
-    date: '03-26-2018',
-    time: '3:30pm',
+    date: new Date(),
     type: 'dry diaper'
   },
   {
     id: 4,
     activity: 'potty',
     icon: 'theme',
-    date: '03-23-2018',
-    time: '3:33pm',
+    date: new Date(),
+    type: 'potty attempt'
+  },
+  {
+    id: 5,
+    activity: 'potty',
+    icon: 'theme',
+    date: new Date(),
+    type: 'wet diaper'
+  },
+  {
+    id: 6,
+    activity: 'potty',
+    icon: 'fire',
+    date: new Date(),
+    type: 'BM diaper'
+  },
+  {
+    id: 7,
+    activity: 'potty',
+    icon: 'theme',
+    date: new Date(),
+    type: 'dry diaper'
+  },
+  {
+    id: 8,
+    activity: 'potty',
+    icon: 'theme',
+    date: new Date(),
+    type: 'potty attempt'
+  },
+  {
+    id: 9,
+    activity: 'potty',
+    icon: 'theme',
+    date: new Date(),
+    type: 'wet diaper'
+  },
+  {
+    id: 10,
+    activity: 'potty',
+    icon: 'fire',
+    date: new Date(),
+    type: 'BM diaper'
+  },
+  {
+    id: 11,
+    activity: 'potty',
+    icon: 'theme',
+    date: new Date(),
+    type: 'dry diaper'
+  },
+  {
+    id: 12,
+    activity: 'potty',
+    icon: 'theme',
+    date: new Date(),
     type: 'potty attempt'
   }
 ];
